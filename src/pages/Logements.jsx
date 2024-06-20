b7e9d6931f58b207cdf722611fbb1dfc63a58bb7
Elodie <elodie.benard76@gmail.com>
2024-06-20 17:35:13 +0200

    maj page logement - ajout partie description

--- src/pages/Logements.jsx
+++ src/pages/Logements.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import logements from "../data/logements.json";
+import DropdownBtn from "../components/DropdownBtn";
 
 const logementId = (id)=>{
     return logements.find((logement)=>logement.id === id);
@@ -10,16 +11,38 @@ const Hebergement = () => {
     const{id} = useParams();
     const logement = logementId(id);
     const imagesLogement = logement.pictures;
+    const hostName = logement.host.name;
+    const hostPicture = logement.host.picture;
     return (
         <div className='hebergement'>
 
-            <h1>hebergements kasa id: {id}</h1>
+            
             <div className="pictures">
                 {imagesLogement.map((imageLogement,index)=>{
                 return (
-                    <img src={imageLogement} alt={imageLogement.title} key={(imageLogement,index)}/>
+                    <img className='img-logement' src={imageLogement} alt={imageLogement.title} key={(imageLogement,index)}/>
                 )
                 })}
+            </div>
+            <div className='logements-content'>
+                <div className='description'>
+                    <div className='logement-box'>
+                        <p>{logement.title}</p>
+                        <p>{logement.location}</p>
+                    </div>
+                    <div className='host-box'>
+                        <p>{hostName}</p>
+                        <img src={hostPicture} alt={hostName} /> */
+                    </div>
+                </div>
+                <div className='info-box'>
+                    <div className='tags'>tags</div>
+                    <div className='rating'>ratings stars</div>
+                </div>
+                <div className='dropdown-box'>
+                    <DropdownBtn title="Description" />
+                    <DropdownBtn title="Équipement" />
+                </div>
             </div>
             
         </div>
